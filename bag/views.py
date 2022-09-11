8a1e361451f6a68defbb8b7308f7412ceb4af103
antoniog675 <antoniogomez1@hotmail.co.uk>
2022-09-11 09:40:30 +0000

    product detail loads displaying quantity and add to bag feature, not working at the moment

--- bag/views.py
+++ bag/views.py
@@ -7,3 +7,7 @@ def view_bag(request):
     """A view to return the bag page"""
     
     return render(request, 'bag/bag.html')
+
+
+def add_to_bag(request, item_id):
+    """Add a specified quantity of products into the bag"""
